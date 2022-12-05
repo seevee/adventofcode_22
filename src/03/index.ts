ab6ac6cfcca366daf2432cdb884b7dd905883c1a
Chris Vincent <c.m.vincent@pm.me>
2022-12-05 15:03:53 -0700

    feat: day 03 part 2 + refactor

--- src/03/index.ts
+++ src/03/index.ts
@@ -9,7 +9,9 @@ const charPriority = (char: string): number => {
   return charCode > 96 ? charCode - 96 : charCode - 38
 }
 
-const uniqueCompartments = (rucksack: string[]): [string[], string[]] => {
+type RucksackCompartments = [string[], string[]]
+
+const uniqueCompartments = (rucksack: string[]): RucksackCompartments => {
   const halfway = Math.ceil(rucksack.length / 2)
   return [
     [...new Set(rucksack.slice(0, halfway))],
@@ -17,7 +19,7 @@ const uniqueCompartments = (rucksack: string[]): [string[], string[]] => {
   ]
 }
 
-const sharedChar = (rucksack: [string[], string[]]): string => {
+const sharedChar = (rucksack: RucksackCompartments): string => {
   for (const i in rucksack[0]) {
     if (rucksack[1].includes(rucksack[0][i]))
       return rucksack[0][i]
@@ -28,4 +30,21 @@ const sharedChar = (rucksack: [string[], string[]]): string => {
 // Part 1 Answer
 console.log(formattedInput.reduce((acc, rucksack) => {
   return acc + charPriority(sharedChar(uniqueCompartments(rucksack)))
+}, 0))
+
+type GroupRucksacks = [string[], string[], string[]]
+
+const groupSharedChar = (group: GroupRucksacks): string => {
+  for (const i in group[0]) {
+    if ([group[1], group[2]].every(rucksack => rucksack.includes(group[0][i])))
+      return group[0][i]
+  }
+  return ''
+}
+
+// Part 2 Answer
+console.log(formattedInput.reduce((acc, _, i) => {
+  if (i % 3 != 0)
+    return acc
+  return acc + charPriority(groupSharedChar(formattedInput.slice(i, i + 3) as GroupRucksacks))
 }, 0))
