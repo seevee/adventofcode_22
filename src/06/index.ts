33e781000f2a0fc6275a7b9e05209d81784b04e7
Chris Vincent <c.m.vincent@pm.me>
2022-12-06 13:30:07 -0700

    refactor: cleanup day 06

--- src/06/index.ts
+++ src/06/index.ts
@@ -4,12 +4,9 @@ const file = await readFileToString(import.meta.url, 'input')
 const fileInput: string[] = file.split('\n').filter(x => !!x)[0].split('')
 
 const processChars = (chars: string[], numDistinct: number) => {
-  for (let i = 0; i < chars.length + 1; i++) {
-    if (i < numDistinct) continue
+  for (let i = numDistinct; i <= chars.length; i++) {
     const marker = chars.slice(i - numDistinct, i)
-    if ([...new Set(marker)].length === marker.length) {
-      return i
-    }
+    if ([...new Set(marker)].length === marker.length) return i
   }
 }
 
