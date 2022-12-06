8aa95fbe1e415b92423f41e6391693ada4af2f83
Chris Vincent <c.m.vincent@pm.me>
2022-12-06 10:53:56 -0700

    refactor: cleanup day 04

--- src/04/index.ts
+++ src/04/index.ts
@@ -1,32 +1,26 @@
 import { readFileToString } from '../util/file.js'
 
-type Range = [number, number]
+type Range = [number, number] // first minimum, second maximum
 type RangePair = [Range, Range]
 
 const file = await readFileToString(import.meta.url, 'input')
 const fileInput: RangePair[] = file
   .split('\n')
   .filter(s => !!s)
-  .map(s => s.split(',').map(s => s.split('-').map(x => parseInt(x)) as Range) as RangePair)
+  .map(s => s.split(',').map(s => s.split('-').map(x => parseInt(x))) as RangePair)
+
+const rangesContained = (pair: RangePair) => ((
+  pair[0][0] <= pair[1][0] && pair[0][1] >= pair[1][1]
+) || (
+  pair[1][0] <= pair[0][0] && pair[1][1] >= pair[0][1]
+))
 
 // Part 1 Answer
-console.log(fileInput.filter((rangePair: RangePair) => {
-  return ((
-    rangePair[0][0] <= rangePair[1][0] &&
-      rangePair[0][1] >= rangePair[1][1]
-  ) || (
-    rangePair[1][0] <= rangePair[0][0] &&
-      rangePair[1][1] >= rangePair[0][1]
-  ))
-}).length)
+console.log(fileInput.filter(rangesContained).length)
+
+const rangesOverlap = (pair: RangePair) => (
+  pair[0][0] <= pair[1][1] && pair[0][1] >= pair[1][0]
+)
 
 // Part 2 Answer
-console.log(fileInput.filter((rangePair: RangePair) => {
-  return ((
-    rangePair[0][0] <= rangePair[1][1] &&
-      rangePair[0][1] >= rangePair[1][0]
-  ) || (
-    rangePair[1][0] <= rangePair[0][1] &&
-      rangePair[1][1] >= rangePair[0][0]
-  ))
-}).length)
+console.log(fileInput.filter(rangesOverlap).length)
