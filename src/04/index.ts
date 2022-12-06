24f15e5d289193203f08657a20f69917edf2c95f
Chris Vincent <c.m.vincent@pm.me>
2022-12-06 10:16:04 -0700

    feat: day 04 complete

--- src/04/index.ts
+++ src/04/index.ts
@@ -1,4 +1,32 @@
 import { readFileToString } from '../util/file.js'
 
+type Range = [number, number]
+type RangePair = [Range, Range]
+
 const file = await readFileToString(import.meta.url, 'input')
-const fileInput: string[][] = file.split('\n').filter(str => !!str).map(s => s.split(''))
+const fileInput: RangePair[] = file
+  .split('\n')
+  .filter(s => !!s)
+  .map(s => s.split(',').map(s => s.split('-').map(x => parseInt(x)) as Range) as RangePair)
+
+// Part 1 Answer
+console.log(fileInput.filter((rangePair: RangePair) => {
+  return ((
+    rangePair[0][0] <= rangePair[1][0] &&
+      rangePair[0][1] >= rangePair[1][1]
+  ) || (
+    rangePair[1][0] <= rangePair[0][0] &&
+      rangePair[1][1] >= rangePair[0][1]
+  ))
+}).length)
+
+// Part 2 Answer
+console.log(fileInput.filter((rangePair: RangePair) => {
+  return ((
+    rangePair[0][0] <= rangePair[1][1] &&
+      rangePair[0][1] >= rangePair[1][0]
+  ) || (
+    rangePair[1][0] <= rangePair[0][1] &&
+      rangePair[1][1] >= rangePair[0][0]
+  ))
+}).length)
